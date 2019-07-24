7bcc4326a902dfd216bc362739cfa961f046438f
Christian Banse <oxisto@aybaze.com>
2019-07-24 18:44:31 +0200

    Added log level to log writer

--- log.go
+++ log.go
@@ -1,20 +1,19 @@
 package httputil
 
-
 import (
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
 
-
-// DebugLogWriter implements io.Writer and writes all incoming text out to log level info.
-type DebugLogWriter struct {
+// LogWriter implements io.Writer and writes all incoming text out to the specified log level.
+type LogWriter struct {
+	Level     logrus.Level
 	Component string
 }
 
-func (d DebugLogWriter) Write(p []byte) (n int, err error) {
-	logrus.WithField("component", d.Component).Debug(strings.TrimRight(string(p), "\n"))
+func (d LogWriter) Write(p []byte) (n int, err error) {
+	logrus.WithField("component", d.Component).Log(d.Level, strings.TrimRight(string(p), "\n"))
 
 	return len(p), nil
 }
