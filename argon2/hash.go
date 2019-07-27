13d38199a752b7102893d9b544a49af65334d311
Christian Banse <oxisto@aybaze.com>
2019-07-27 18:02:19 +0200

    Implemented TestCompareHashAndPassword. Fixes #1

--- argon2/hash.go
+++ argon2/hash.go
@@ -2,10 +2,24 @@ package argon2
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 	"fmt"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
+)
+
+var (
+	// ErrMismatchedHashAndPassword is returned when password and hash do not match
+	ErrMismatchedHashAndPassword = errors.New("argon2: encoded is not the hash of the given password")
+
+	// ErrIncompatibleVersion is returned when hash versions are not compatible
+	ErrIncompatibleVersion = errors.New("argon2: version is not compatible")
+
+	// ErrInvalidHash is returned when the encoded hash has an invalid format
+	ErrInvalidHash = errors.New("argon2: encoded hash has invalid format")
 )
 
 // IDParams holds all necessary parameters to configure argon2id
@@ -31,18 +45,23 @@ func init() {
 }
 
 // GenerateFromPassword generates a new argon2id encoded hash from the specified password
-func GenerateFromPassword(password []byte) (string, error) {
+func GenerateFromPassword(password []byte) ([]byte, error) {
 	return GenerateFromPasswordWithParams(password, DefaultParams)
 }
 
 // GenerateFromPasswordWithParams generates a new argon2id encoded hash from the specified password using the parameters specified
-func GenerateFromPasswordWithParams(password []byte, params IDParams) (string, error) {
+func GenerateFromPasswordWithParams(password []byte, params IDParams) ([]byte, error) {
 	// generate the salt
 	salt, err := GenerateRandomBytes(params.SaltLength)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
+	return GenerateFromPasswordWithParamsAndSalt(password, params, salt)
+}
+
+// GenerateFromPasswordWithParamsAndSalt generates a new argon2id encoded hash from the specified password and salt using the parameters specified
+func GenerateFromPasswordWithParamsAndSalt(password []byte, params IDParams, salt []byte) ([]byte, error) {
 	// calculate the hash
 	hash := argon2.IDKey(password, salt,
 		params.Iterations,
@@ -63,7 +82,7 @@ func GenerateFromPasswordWithParams(password []byte, params IDParams) (string, e
 		b64hash,
 	)
 
-	return encoded, nil
+	return []byte(encoded), nil
 }
 
 // GenerateRandomBytes generates random bytes of the specified size
@@ -76,4 +95,67 @@ func GenerateRandomBytes(size uint32) ([]byte, error) {
 	}
 
 	return salt, nil
+}
+
+// CompareHashAndPassword compares an argon2id hashed and encoded password with its possible
+// plaintext equivalent. Returns nil on success, or an error on failure.
+func CompareHashAndPassword(encoded []byte, password []byte) error {
+	params, salt, _, err := newFromHash(encoded)
+	if err != nil {
+		return err
+	}
+
+	other, err := GenerateFromPasswordWithParamsAndSalt(password, *params, salt)
+	if err != nil {
+		return err
+	}
+
+	if subtle.ConstantTimeCompare(encoded, other) == 1 {
+		return nil
+	}
+
+	return ErrMismatchedHashAndPassword
+}
+
+func newFromHash(encoded []byte) (params *IDParams, salt []byte, hash []byte, err error) {
+	var (
+		version int
+	)
+
+	parts := strings.Split(string(encoded), "$")
+
+	// hash starts with $argonid$
+	if len(parts) != 6 || parts[0] != "" || parts[1] != "argon2id" {
+		return nil, nil, nil, ErrInvalidHash
+	}
+
+	params = &IDParams{}
+
+	if _, err = fmt.Sscanf(parts[2], "v=%d", &version); err != nil {
+		return nil, nil, nil, err
+	}
+
+	if version != argon2.Version {
+		return nil, nil, nil, ErrIncompatibleVersion
+	}
+
+	if _, err = fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d",
+		&params.Memory,
+		&params.Iterations,
+		&params.Parallelism); err != nil {
+		return nil, nil, nil, err
+	}
+
+	if salt, err = base64.RawStdEncoding.DecodeString(parts[4]); err != nil {
+		return nil, nil, nil, err
+	}
+
+	if hash, err = base64.RawStdEncoding.DecodeString(parts[5]); err != nil {
+		return nil, nil, nil, err
+	}
+
+	params.SaltLength = uint32(len(salt))
+	params.KeyLength = uint32(len(hash))
+
+	return params, salt, hash, nil
 }
--- argon2/hash_test.go
+++ argon2/hash_test.go
@@ -1,7 +1,6 @@
 package argon2
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,5 +11,33 @@ func TestGenerateFromPassword(t *testing.T) {
 
 	assert.Nil(t, err)
 
-	fmt.Printf("%v", hash)
+	t.Logf("%v", hash)
+}
+
+func TestCompareHashAndPassword(t *testing.T) {
+	encoded := []byte("$argon2id$v=19$m=65536,t=3,p=2$ekLs6AdWsbtp81gzwAzGIg$4xL21LVn8cL3QrJsfYCytV6DwKtbWvMuMzhxcx2oN9U")
+	password := []byte("test")
+	notPassword := []byte("notPassword")
+
+	params, salt, hash, err := newFromHash(encoded)
+
+	assert.Nil(t, err)
+	assert.True(t, len(salt) == 16)
+
+	assert.Equal(t, uint32(65536), params.Memory)
+	assert.Equal(t, uint32(3), params.Iterations)
+	assert.Equal(t, uint8(2), params.Parallelism)
+
+	assert.Equal(t, uint32(16), params.SaltLength)
+	assert.Equal(t, uint32(32), params.KeyLength)
+
+	t.Logf("Encoded: %v Params: %v Salt: %v Hash: %v", encoded, params, salt, hash)
+
+	err = CompareHashAndPassword(encoded, password)
+
+	assert.Nil(t, err)
+
+	err = CompareHashAndPassword(encoded, notPassword)
+
+	assert.Equal(t, ErrMismatchedHashAndPassword, err)
 }
